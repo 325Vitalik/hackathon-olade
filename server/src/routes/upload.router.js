3c35370f3b679043e8f654f2fe6b9f45bade9ded
maksym <maksym.slobodianyk@binary-studio.com>
2021-02-20 23:34:36 +0200

    Add saving to S3 bucket

--- server/src/routes/upload.router.js
+++ server/src/routes/upload.router.js
@@ -1,26 +1,28 @@
 import { Router } from 'express';
 import multer from 'multer';
-import s3 from '../configs/s3.config.js';
+import {s3Client} from '../configs/config';
 import { v4 as uuidv4 } from 'uuid'
 
 var storage = multer.memoryStorage()
-var upload = multer({storage: storage});
+var upload = multer({ storage: storage });
 
- 
+
 const router = new Router();
 router.post('/', upload.single("file"), (req, res) => {
-	const s3Client = s3.s3Client;
-	const params = s3.uploadParams;
-	
-	params.Key = `${uuidv4()}.png`;
-	params.Body = req.file.buffer;
-		
+
+	const params = {
+		Bucket: 'pet-finder-lviv',
+		Key: `${uuidv4()}.png`,
+		Body: req.file.buffer,
+		ACL: 'public-read'
+	};
+
 	s3Client.upload(params, (err, data) => {
 		if (err) {
-			res.status(500).json({error:"Error -> " + err});
+			res.status(500).json({ error: "Error -> " + err });
 		}
-		res.json({message: `File uploaded successfully! -> keyname = ${data.Location}`});
+		res.json({ link:data.Location });
 	});
 });
- 
+
 export { router as uploadRouter };
