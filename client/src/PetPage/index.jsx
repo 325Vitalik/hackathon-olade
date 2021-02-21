c4101de28d97927641c830fe5d977162a66e2a92
maksym <maksym.slobodianyk@binary-studio.com>
2021-02-21 07:50:52 +0200

    Add spinner

--- client/src/PetPage/index.jsx
+++ client/src/PetPage/index.jsx
@@ -9,24 +9,29 @@ import {
   Image,
   Container,
   Grid,
-  Button
+  Button,
+  Loader,
 } from 'semantic-ui-react';
 import { config } from "../config";
 import Header from '../Shared/Header'
 import { getAuthHeader } from "../Auth/firebaseService";
 import { bindActionCreators } from 'redux';
+import InfoModal from './InfoModal';
 
 const PetPage = () => {
 
   const [card, setCard] = useState({});
   const [loading, setIsLoading] = useState(true);
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
-  
+
   useEffect(() => {
     const url = new URL(`${config.hostname}/pet/${id}`);
-    fetch(url,{		headers: {
-			Authorization: getAuthHeader(),
-		},}).then(async (response) => {
+    fetch(url, {
+      headers: {
+        Authorization: getAuthHeader(),
+      },
+    }).then(async (response) => {
       if (response.ok) {
         const c = await response.json()
         console.log(c);
@@ -36,82 +41,93 @@ const PetPage = () => {
         console.log(response);
       }
     });
-  },[]);
+  }, []);
 
-  return loading? (<></>):(
+  return loading ? (<></>) : (
     <>
       <Header />
       <Container className={styles.mainContainer}>
         <Grid container textAlign="center" style={{ paddingTop: 30 }}>
-
-          <Grid.Row>
-            <Grid.Column width={7}>
-              <Image centered src={card.animalImageLink} size="middle" rounded />
-
-              <Button className={styles.searchButton} onClick={()=>{}} color="primary" fluid size="large">
-								Знайти схожчі за фото
+          {loading ? (
+            <Loader type="Puff" color="#2b2b2bd9" height={100} width={100} />
+          ) : (
+              <>
+                <Grid.Row>
+                  <Grid.Column width={7}>
+                    <Image centered src={card.animalImageLink} size="middle" rounded />
+                    <Grid.Column>
+                      <Button className={styles.searchButton} onClick={() => { }} color="primary" fluid size="large">
+                        Знайти схожчі за фото
 							</Button>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <div className={styles.profileInfoTextTitle}>
-                Кличка:
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Button className={styles.searchButton} onClick={() => setShowModal(true)} color="green" fluid size="large">
+                        Зконтактувати з заявником
+							</Button>
+                    </Grid.Column>
+
+                  </Grid.Column>
+                  <Grid.Column width={3}>
+                    <div className={styles.profileInfoTextTitle}>
+                      Кличка:
         </div>
-              <div className={styles.profileInfoTextTitle}>
-                Вид тварини:
+                    <div className={styles.profileInfoTextTitle}>
+                      Вид тварини:
         </div>
-              <div className={styles.profileInfoTextTitle}>
-                Порода:
+                    <div className={styles.profileInfoTextTitle}>
+                      Порода:
         </div>
-              <div className={styles.profileInfoTextTitle}>
-                Колір:
+                    <div className={styles.profileInfoTextTitle}>
+                      Колір:
         </div>
-              <div className={styles.profileInfoTextTitle}>
-                Дата втрати:
+                    <div className={styles.profileInfoTextTitle}>
+                      Дата втрати:
         </div>
-              <div className={styles.profileInfoTextTitle}>
-                Винагорода:
+                    <div className={styles.profileInfoTextTitle}>
+                      Винагорода:
         </div>
-              <div className={styles.profileInfoTextTitle}>
-                Опис:
+                    <div className={styles.profileInfoTextTitle}>
+                      Опис:
         </div>
-            </Grid.Column>
-            <Grid.Column width={6}>
-              <div className={styles.profileInfoText}>
-                {card.animalName}
+                  </Grid.Column>
+                  <Grid.Column width={6}>
+                    <div className={styles.profileInfoText}>
+                      {card.animalName}
+                    </div>
+                    <div className={styles.profileInfoText}>
+                      {card.animalType === 'dog' ? 'собака' : 'кіт'}
+                    </div>
+                    <div className={styles.profileInfoText}>
+                      {card.animalBreed}
+                    </div>
+                    <div className={styles.profileInfoText}>
+                      {card.animalColour}
+                    </div>
+                    <div className={styles.profileInfoText}>
+                      {card.lossDate}
+                    </div>
+                    <div className={styles.profileInfoText}>
+                      {card.award} 	₴
         </div>
-              <div className={styles.profileInfoText}>
-              {card.animalType === 'dog' ? 'собака' : 'кіт'}
-        </div>
-              <div className={styles.profileInfoText}>
-              {card.animalBreed}
-        </div>
-              <div className={styles.profileInfoText}>
-                {card.animalColour}
-        </div>
-              <div className={styles.profileInfoText}>
-                {card.lossDate}
-        </div>
-              <div className={styles.profileInfoText}>
-                {card.award} 	₴
-        </div>
-              <div className={styles.profileInfoText}>
-                {card.animalDescription}
-        </div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <div style={{ width: "100%", height: "300px" }}>
-              <Map center={card.lossLocationCoordinates} radius={card.allowedRadius} zoom={13-0.45*(card.allowedRadius/1000)} />
-            </div>
-          </Grid.Row>
+                    <div className={styles.profileInfoText}>
+                      {card.animalDescription}
+                    </div>
+                  </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                  <div style={{ width: "100%", height: "300px" }}>
+                    <Map center={card.lossLocationCoordinates} radius={card.allowedRadius} zoom={13 - 0.45 * (card.allowedRadius / 1000)} />
+                  </div>
+                </Grid.Row>
+              </>
+
+            )}
+
         </Grid>
       </Container>
+      {showModal ? <InfoModal userId={card.userId} setClose={() => { setShowModal(false) }}></InfoModal> : <></>}
     </>
   )
-};
-
-PetPage.propTypes = {
-
 };
 
 PetPage.defaultProps = {
