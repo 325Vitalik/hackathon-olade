855cca3d58504e06bb87a430823ecf0f3454641b
maksym <maksym.slobodianyk@binary-studio.com>
2021-02-21 05:47:28 +0200

    Format code

--- client/src/Profile/index.jsx
+++ client/src/Profile/index.jsx
@@ -26,22 +26,22 @@ const Profile = ({ user }) => {
 
   const update = async () => {
     const url = new URL(`${config.hostname}/user/update`);
-    const updatedUser = {...user, firstName,lastName,email,phone}
+    const updatedUser = { ...user, firstName, lastName, email, phone }
     try {
       fetch(url, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ user: updatedUser }),
-			}).then(async (postResponse) => {
-				if (postResponse.ok) {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ user: updatedUser }),
+      }).then(async (postResponse) => {
+        if (postResponse.ok) {
 
           toggleEditMode();
-				} else {
-					console.log(postResponse);
-				}
-			});
+        } else {
+          console.log(postResponse);
+        }
+      });
     } catch (e) {
       console.log(e);
     }
@@ -52,62 +52,98 @@ const Profile = ({ user }) => {
     <Container className={styles.mainContainer}>
       <Grid container textAlign="center" style={{ paddingTop: 30 }}>
         <Grid.Row>
-          <Grid.Column width={6}>
+          <Grid.Column width={7}>
             <Image centered src={'mock-avatar.png'} size="small" rounded />
             <br />
             {!editMode && (
               <p className={styles.editButton} onClick={toggleEditMode}>Редагувати профіль</p>)}
             <br />
           </Grid.Column>
-          <Grid.Column width={10}>
-            <Form onSubmit={update}>
-              <Form.Input
-                icon="address card"
-                iconPosition="left"
-                placeholder="Ім'я користувача"
-                type="text"
-                value={user.firstName}
-                disabled={!editMode}
-                onChange={ev => setFirstName(ev.target.value)}
-              />
-              <Form.Input
-                icon="address card"
-                iconPosition="left"
-                placeholder="Прізвище користувача"
-                type="text"
-                value={lastName}
-                disabled={!editMode}
-                onChange={ev => setLastName(ev.target.value)}
-              />
-              <Form.Input
-                icon="at"
-                iconPosition="left"
-                placeholder="Електронна пошта"
-                type="email"
-                value={email}
-                disabled={!editMode}
-                onChange={ev => setEmail(ev.target.value)}
-              />
-              <Form.Input className={styles.infoInput}
-                icon="phone"
-                iconPosition="left"
-                placeholder="Номер телефону"
-                type="text"
-                value={phone}
-                disabled={!editMode}
-                onChange={ev => setPhone(ev.target.value)}
-              />
-              <br />
-              {editMode && (
-                <div className={styles.buttonContainer}>
-                  <Button color="teal" type="submit">Зберегти</Button>
-                  <Button className={styles.button} onClick={() => toggleEditMode()}>Відмінити</Button>
-                </div>
-              )}
-            </Form>
+          {!editMode && (
+            <>
+              <Grid.Column width={3}>
+                <div className={styles.profileInfoTextTitle}>
+                  Ім'я:
+        </div>
+                <div className={styles.profileInfoTextTitle}>
+                  Прізвище:
+        </div>
+                <div className={styles.profileInfoTextTitle}>
+                  Email:
+        </div>
+                <div className={styles.profileInfoTextTitle}>
+                  Номер телефону:
+        </div>
+              </Grid.Column>
+              <Grid.Column width={6}>
+                <div className={styles.profileInfoText}>
+                  {firstName}
+        </div>
+                <div className={styles.profileInfoText}>
+                {lastName}
+        </div>
+                <div className={styles.profileInfoText}>
+                {email}
+        </div>
+                <div className={styles.profileInfoText}>
+                {phone}
+        </div>
+              </Grid.Column>
+            </>
+          )}
+          {editMode && (
+            <>
+              <Grid.Column width={9}>
+                <Form onSubmit={update}>
+                  <Form.Input
+                    icon="address card"
+                    iconPosition="left"
+                    placeholder="Ім'я користувача"
+                    type="text"
+                    value={firstName}
+                    disabled={!editMode}
+                    onChange={ev => setFirstName(ev.target.value)}
+                  />
+                  <Form.Input
+                    icon="address card"
+                    iconPosition="left"
+                    placeholder="Прізвище користувача"
+                    type="text"
+                    value={lastName}
+                    disabled={!editMode}
+                    onChange={ev => setLastName(ev.target.value)}
+                  />
+                  <Form.Input
+                    icon="at"
+                    iconPosition="left"
+                    placeholder="Електронна пошта"
+                    type="email"
+                    value={email}
+                    disabled={!editMode}
+                    onChange={ev => setEmail(ev.target.value)}
+                  />
+                  <Form.Input className={styles.infoInput}
+                    icon="phone"
+                    iconPosition="left"
+                    placeholder="Номер телефону"
+                    type="text"
+                    value={phone}
+                    disabled={!editMode}
+                    onChange={ev => setPhone(ev.target.value)}
+                  />
+                  <br />
+                  {editMode && (
+                    <div className={styles.buttonContainer}>
+                      <Button color="teal" type="submit">Зберегти</Button>
+                      <Button className={styles.button} onClick={() => toggleEditMode()}>Відмінити</Button>
+                    </div>
+                  )}
+                </Form>
 
 
-          </Grid.Column>
+              </Grid.Column>
+            </>
+          )}
         </Grid.Row>
         <Grid.Row>
           <p className={styles.myAnnouncements}>Мої оголошення</p>
